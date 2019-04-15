ad7b29403617bf988b33d455e2dce924f953c65a
Sarah0711 <sarahjmi07@gmail.com>
2019-04-15 12:05:42 +0200

    Added a class

---
+++ eps_bandit.py
@@ -0,0 +1,103 @@
+import numpy as np
+
+class eps_bandit:
+    '''
+    epsilon-greedy k-bandit problem
+
+    Inputs
+    =====================================================
+    k: number of arms (int)
+    eps: probability of random action 0 < eps < 1 (float)
+    iters: number of steps (int)
+    mu: set the average rewards for each of the k-arms.
+        Set to "random" for the rewards to be selected from
+        a normal distribution with mean = 0.
+        Set to "sequence" for the means to be ordered from
+        0 to k-1.
+        Pass a list or array of length = k for user-defined
+        values.
+    '''
+
+    def __init__(self, k, eps, iters):
+        # Number of arms
+        self.k = k
+        # Search probability
+        self.eps = eps
+        # Number of iterations
+        self.iters = iters
+        # Step count
+        self.n = 0
+        # Step count for each arm
+        self.k_n = np.zeros(k)
+        # Total mean reward
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        # Mean reward for each arm
+        self.k_reward = np.zeros(k)
+
+    def calc_mu(self, a):
+        if a == 0:
+            low = 1
+            high =3
+        elif a == 1:
+            low = -3
+            high= 8
+        elif a == 2:
+            low = 2
+            high = 5
+        elif a == 3:
+            low=-2
+            high=6
+        elif a ==4:
+            low=3
+            high=4
+        elif a ==5:
+            low=-2
+            high=2
+        else:
+            print "Value undefined"
+
+
+        sampled_value = np.random.uniform(low,high)
+        return sampled_value
+
+    def pull(self):
+        # Generate random number
+        p = np.random.rand()
+        if self.eps == 0 and self.n == 0:
+            a = np.random.choice(self.k)
+        elif p < self.eps:
+            # Randomly select an action
+            a = np.random.choice(self.k)
+        else:
+            # Take greedy action
+            a = np.argmax(self.k_reward)
+        sv = self.calc_mu(a)
+        reward = np.random.normal(sv, 1)
+
+        # Update counts
+        self.n += 1
+        self.k_n[a] += 1
+
+        # Update total
+        self.mean_reward = self.mean_reward + (
+                reward - self.mean_reward) / self.n
+
+        # Update results for a_k
+        self.k_reward[a] = self.k_reward[a] + (
+                reward - self.k_reward[a]) / self.k_n[a]
+
+
+
+    def run(self):
+        for i in range(self.iters):
+            self.pull()
+            self.reward[i] = self.mean_reward
+
+    def reset(self):
+        # Resets results while keeping settings
+        self.n = 0
+        self.k_n = np.zeros(self.k)
+        self.mean_reward = 0
+        self.reward = np.zeros(self.iters)
+        self.k_reward = np.zeros(self.k)
